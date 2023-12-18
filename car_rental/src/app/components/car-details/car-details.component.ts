239b316d308458961a7e9768ede7af6aeba09ef8
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2023-12-18 20:14:49 +0100

    Poprawa details

--- car_rental/src/app/components/car-details/car-details.component.ts
+++ car_rental/src/app/components/car-details/car-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { CarData } from 'src/app/models/car-data';
@@ -10,33 +10,12 @@ import { CarService } from 'src/app/services/car.service';
   styleUrls: ['./car-details.component.css']
 })
 export class CarDetailsComponent {
-  car:CarData|undefined;
-  //Id:string|null = "";
-  constructor(private route:ActivatedRoute, private carService:CarService){
+  @Input() car?:CarData;
+  constructor(){
    
   }
   ngOnInit() {
-    let Id:number = 0;  
-    this.route.paramMap.subscribe(params =>{
-        Id = Number(params.get('id'));
-        this.carService.getOneCar(Id).subscribe((result:any) => {
-          this.car = new CarData(
-                        result.id,
-                        result.manufacturer,
-                        result.model,
-                        result.date_of_manufacture,
-                        result.available_count,
-                        result.rental_cost,
-                        result.seats_count,
-                        result.gearbox,
-                        result.type
-                      );
-        });   
-    });
     
   }
-
-    
-   
 }
 
